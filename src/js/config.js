c19eff2445ca9e50641d0d3f848a2aa5c7768315
yukki.dev <yukki@duck.com>
2026-06-01 20:10:40 -0400

    Remove yukki.dev domain from config and nginx files

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@
 // Edit this file to set your API key (must match the API_KEY in src/backend/.env)
 const CLIENT_CONFIG = {
     API_KEY: 'ohboy3AM!123123123',
-    API_BASE_URL: 'https://api.yukki.dev/todoapp'
+    API_BASE_URL: 'http://localhost:8000/todoapp'
 };
